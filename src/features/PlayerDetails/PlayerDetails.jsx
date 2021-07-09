d99ad4d7343d46d83a18e574b7d63c22cc0c9436
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-09 20:18:34 +0200

    CLEAN, logs

--- src/features/PlayerDetails/PlayerDetails.jsx
+++ src/features/PlayerDetails/PlayerDetails.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 const PlayerDetails = ({ match: { params } }) => {
 	const { player, status } = useSelector(selectPlayer);
-	console.log(`player`, player, status);
+
 	const dispatch = useDispatch();
 
 	useEffect(() => {
