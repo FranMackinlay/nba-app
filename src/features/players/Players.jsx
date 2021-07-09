2571f5b15d9611faf50dd46126bb7c5dccd4da45
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-09 16:50:52 +0200

    REFACTOR, players component with card

--- src/features/players/Players.jsx
+++ src/features/players/Players.jsx
@@ -1,10 +1,8 @@
-import { Box, Flex, Image, Spinner } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPlayers, selectPlayers } from './playersSlice';
-import styles from './Players.module.css';
-import { Link } from 'react-router-dom';
-import { PLAYER_IMG_URL } from '../../constants/api.constants';
+import { Flex, Spinner } from '@chakra-ui/react';
+import Card from '../Card/Card';
 
 export function Players() {
   const { players, status } = useSelector(selectPlayers);
@@ -20,26 +18,7 @@ export function Players() {
         ) :
           (
             players?.length && players.map(player => (
-              <Link to={`/player/${player.id}`} key={player.id}>
-                <Box className={styles.playerCard} m={{ base: "5px 0", md: "5px" }} maxW={{ base: "", md: "345px" }} flex={{ base: "1 1 auto", md: "" }} textAlign="center" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                  <Box p="6">
-                    <Image m="10px auto" borderRadius="lg" src={`${PLAYER_IMG_URL}/${player.last_name}/${player.first_name}`} fallbackSrc="https://res.cloudinary.com/ducmbpfde/image/upload/v1625836216/nba_logo_npw9re.png" alt="player_image_not_found_😢" />
-                    <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
-                      {player.first_name} {player.last_name}
-                    </Box>
-                    <Box>
-                      <Box as="span" color="gray.600" fontSize="sm" ml={2}>
-                        Position: {player.position}
-                      </Box>
-                    </Box>
-                    <Box>
-                      <Box as="span" color="gray.600" fontSize="sm" ml={2}>
-                        Team: {player.team.full_name}
-                      </Box>
-                    </Box>
-                  </Box>
-                </Box>
-              </Link>
+              <Card player={player} key={player.id}></Card>
             ))
           )
         }
