61195dd9b4e3941112cb72130671f49e97906b3d
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-09 16:51:01 +0200

    WIP, player details component

--- src/features/PlayerDetails/PlayerDetails.jsx
+++ src/features/PlayerDetails/PlayerDetails.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPlayer, selectPlayer } from '../players/playerSlice';
 
-const PlayerDetails = () => {
+const PlayerDetails = ({match: {params}}) => {
+
+  const {player, status } = useSelector(selectPlayer);
+  console.log(`player, status`, player, status);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPlayer(params.id));
+  }, [dispatch, params])
+
   return (
     <div>
 
