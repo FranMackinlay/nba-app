59a2aa869994c9e554930d5019ea66d6eaa6aae4
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-09 16:29:09 +0200

    ADD, players to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import playersReducer from '../features/players/playersSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    players: playersReducer,
   },
 });
