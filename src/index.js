a5546a290246e4ac36bbb169cf7052a44e645d42
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-09 16:28:57 +0200

    ADD, chakra UI and react router

--- src/index.js
+++ src/index.js
@@ -5,12 +5,28 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { ChakraProvider } from "@chakra-ui/react"
+import PlayerDetails from './features/PlayerDetails/PlayerDetails';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <ChakraProvider>
+          <Switch>
+            <Route path="/player/:id" component={PlayerDetails} />
+            <Route path="/" component={App} />
+          </Switch>
+        </ChakraProvider>
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
