66005df4c57a8ec1853bca4196de3e340958ca68
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-09 16:28:26 +0200

    ADD, app main component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Players } from './features/players/Players';
 import './App.css';
+import { Box, Image } from '@chakra-ui/react';
+import FormComponent from './features/Form/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Box className="App-header" mx={{ base: "10px", md: "0" }}>
+        <Image mt={30} src={'https://res.cloudinary.com/ducmbpfde/image/upload/v1625836216/nba_logo_npw9re.png'} className="App-logo" alt="logo" />
+        <Box w="100%">
+          <FormComponent></FormComponent>
+        </Box>
+        <Players />
+      </Box>
     </div>
   );
 }
